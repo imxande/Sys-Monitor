44c69aae159ad9a71f1dcafca495c4ac99a7832f
imxande <alerodriguezlarosa@gmail.com>
2025-04-12 09:12:59 -0400

    Add cpu layout to resources tab

--- includes/resourcesTab.h
+++ includes/resourcesTab.h
@@ -1,21 +1,35 @@
 #ifndef RESOURCES_TAB_H
 #define RESOURCES_TAB_H
 
-#include <QWidget>
-#include <QVBoxLayout>
+#include "resourceMonitor.h"
+#include <QBoxGroup>
 #include <QQuickWidget>
+#include <QWidget>
 
-/* @brief ResourceTab provides a real-time visual monitoring of overall system metrics.
- * Displays CPU Usage, Memory and Swap, Network Activity and Disk I/O live graphs.
+/* @brief ResourceTab provides a real-time visual monitoring of overall system
+ * metrics. Displays CPU Usage, Memory and Swap, Network Activity and Disk I/O
+ * live graphs.
  */
 class ResourcesTab : public QWidget {
-  // for signals and slots 
+  // for signals and slots
   Q_OBJECT
 
 public:
-  explicit ResourcesTab(QWidget *parent = nullptr); 
+  explicit ResourcesTab(QWidget *parent = nullptr);
   ~ResourcesTab();
- 
+
+  /* @brief Load resources tab layout
+   */
+  void setResourcesLayout();
+
+private:
+  // UI elements
+  QGroupBox *cpuBox;
+  QGroupBox *memSwapBox;
+  QGroupBox *networkBox;
+  QGroupBox *diskBox;
+  QQuickWidget *cpuGraph;
+  ResourceMonitor *monitor;
 };
 
 #endif // RESOURCES_TAB_H
--- src/resourcesTab.cpp
+++ src/resourcesTab.cpp
@@ -1,9 +1,61 @@
 #include "resourcesTab.h"
+#include <QVBoxLayout>
+#include <QQmlContext>
 
 // Init Resource Tab
 ResourcesTab::ResourcesTab(QWidget *parent) : QWidget(parent) {
-  qDebug() << "Resource Tab loaded!";
+  // backend data source
+  monitor = new ResourceMonitor(this);
+
+  // CPU graph
+  cpuGraph->setReziseMode(QQuickWidget::SizeRootObjectToView);
+  cpuGraph->rootContext()->setContextProperty("resourceMonitor", resourceMonitor);
+  cpuGraph->setSource(QUrl("qrc:/qml/cpuGraph.qml"));
+
+  // load resource tab layout
+  setResourcesLayout();
 }
 
 // destructor
 ResourcesTab::~ResourcesTab(){}
+
+// Set resources tab layout
+void ResourcesTab::setResourcesLayout() {
+  // layout
+  QVBoxLayout *resourceslayout = new QVBoxLayout(this);
+
+  // CPU group box
+  cpuBox = new QGroupBox("CPU");
+  cpuBox->setCheckable(true);
+  cpuBox->setChecked(true);
+
+  // Memory/Swap group box
+  memSwapBox = new QGroupBox("Memory and Swap");
+  memSwapBox->setCheckable(true);
+  memSwapBox->setChecked(false);
+
+  // Network group box
+  QGroupBox *networkBox = newQGroupBox("Network");
+  networkBox->setCheckable(true);
+  networkBox->setChecked(false);
+
+  // Disk group box
+  QGroupBox *diskBox = newQGroupBox("Disk");
+  diskBox->setCheckable(true);
+  diskBox->setChecked(false);
+
+
+  // CPU layout
+  QVBoxLayout *cpuLayout = new QVBoxLayout(this);
+  cpuLayout->addWidget(cpuGraph); // add graph widget to CPU layout
+  cpuBox->setLayout(cpuLayout);  // set CPU box layout
+ 
+
+  // add group boxes to resources tab main layout
+  layout->addWidget(cpuBox);       
+  layout->addWidget(memSwapBox);
+  layout->addWidget(networkBox);
+  layout->addWidget(diskBox);
+
+  setLayout(layout);
+}
